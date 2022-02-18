ee3e93248980a4e5272a98f1deb71423c1d7e687
ahmadriffai <rifai0850@gmail.com>
2022-02-18 10:49:52 +0700

    ganti

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'PT. Brilliant Jaya Pondasi') }}</title>
+    <title>PT. Brilliant Jaya Pondasi </title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
