8445854af7454399a227e41e57ae56786413948e
ahmadriffai <rifai0850@gmail.com>
2022-02-18 10:20:01 +0700

    name change

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -20,7 +20,7 @@
     <div class="container mx-3">
         <a class="navbar-brand" href="#">
             <img src="{{asset('assets/img/logo.png')}}" alt="" width="50px">
-            PT. Brillinat Jaya Pondasi
+            PT. Brilliant Jaya Pondasi
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <i class="fas fa-bars"></i>
